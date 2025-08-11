507f8fca836decb42ee9d0e1ff1b3da42fe76640
niasua <nsuarez2410@gmail.com>
2025-08-11 19:59:56 -0300

    adds sales repo

---
+++ EcommerceApi/Repositories/ISaleRepository.cs
@@ -0,0 +1,15 @@
+﻿using EcommerceApi.Models;
+
+namespace EcommerceApi.Repositories
+{
+    public interface ISaleRepository
+    {
+        Task<IEnumerable<Sale>> GetSalesAsync(int pageNumber, int pageSize);
+        Task<int> GetTotalCountAsync();
+        Task<Sale> GetByIdAsync(int id);
+        Task<Sale> GetByIdWithDetailsAsync(int id);
+        Task AddAsync(Sale sale);
+        Task UpdateAsync(Sale sale);
+        Task SoftDeleteAsync(Sale sale);
+    }
+}
---
+++ EcommerceApi/Repositories/SaleRepository.cs
@@ -0,0 +1,60 @@
+﻿using EcommerceApi.Data;
+using EcommerceApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EcommerceApi.Repositories;
+
+public class SaleRepository : ISaleRepository
+{
+    private readonly EcommerceContext _context;
+    public SaleRepository(EcommerceContext context)
+    {
+        _context = context;
+    }
+
+    public async Task AddAsync(Sale sale)
+    {
+        await _context.Sales.AddAsync(sale);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task<Sale> GetByIdAsync(int id)
+    {
+        return await _context.Sales.FindAsync(id);
+    }
+
+    public async Task<Sale> GetByIdWithDetailsAsync(int id)
+    {
+        return await _context.Sales
+            .Include(s => s.SaleProducts)
+                .ThenInclude(sp => sp.Product)
+                    .ThenInclude(p => p.Category)
+            .FirstOrDefaultAsync(s => s.Id == id);
+    }
+
+    public async Task<IEnumerable<Sale>> GetSalesAsync(int pageNumber, int pageSize)
+    {
+        return await _context.Sales
+            .Skip((pageNumber - 1) * pageSize)
+            .Take(pageSize)
+            .ToListAsync();
+    }
+
+    public async Task<int> GetTotalCountAsync()
+    {
+        return await _context.Sales.CountAsync();
+    }
+
+    public async Task SoftDeleteAsync(Sale sale)
+    {
+        sale.isDeleted = true;
+        _context.Sales.Update(sale);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task UpdateAsync(Sale sale)
+    {
+        _context.Sales.Update(sale);
+        await _context.SaveChangesAsync();
+    }
+}
