ada788cab5d972111c3e52ca16a813d0a6d023f7
niasua <nsuarez2410@gmail.com>
2025-08-12 18:12:00 -0300

    adds sale product repo

---
+++ EcommerceApi/Repositories/ISaleProductRepository.cs
@@ -0,0 +1,13 @@
+﻿using EcommerceApi.Models;
+
+namespace EcommerceApi.Repositories;
+
+public interface ISaleProductRepository
+{
+    Task<IEnumerable<SaleProduct>> GetBySaleIdAsync(int saleId);
+    Task<IEnumerable<SaleProduct>> GetByProductIdAsync(int productId);
+    Task<SaleProduct> GetByIdAsync(int saleId, int productId);
+    Task AddAsync(SaleProduct saleProduct);
+    Task UpdateAsync(SaleProduct saleProduct);
+    Task SoftDeleteAsync(SaleProduct saleProduct);
+}
---
+++ EcommerceApi/Repositories/SaleProductRepository.cs
@@ -0,0 +1,52 @@
+﻿using EcommerceApi.Data;
+using EcommerceApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EcommerceApi.Repositories;
+
+public class SaleProductRepository : ISaleProductRepository
+{
+    private readonly EcommerceContext _context;
+    public SaleProductRepository(EcommerceContext context)
+    {
+        _context = context;
+    }
+
+    public async Task AddAsync(SaleProduct saleProduct)
+    {
+        await _context.SaleProducts.AddAsync(saleProduct);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task<SaleProduct> GetByIdAsync(int saleId, int productId)
+    {
+        return await _context.SaleProducts
+            .FirstOrDefaultAsync(sp => sp.SaleId == saleId && sp.ProductId == productId);
+    }
+
+    public async Task<IEnumerable<SaleProduct>> GetByProductIdAsync(int productId)
+    {
+        return await _context.SaleProducts
+            .Where(sp => sp.ProductId == productId && !sp.Product.isDeleted && !sp.Sale.isDeleted)
+            .ToListAsync();
+    }
+
+    public async Task<IEnumerable<SaleProduct>> GetBySaleIdAsync(int saleId)
+    {
+        return await _context.SaleProducts
+            .Where(sp => sp.SaleId == saleId && !sp.Product.isDeleted && !sp.Sale.isDeleted)
+            .ToListAsync();
+    }
+
+    public async Task SoftDeleteAsync(SaleProduct saleProduct)
+    {
+        _context.SaleProducts.Remove(saleProduct);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task UpdateAsync(SaleProduct saleProduct)
+    {
+        _context.SaleProducts.Update(saleProduct);
+        await _context.SaveChangesAsync();
+    }
+}
